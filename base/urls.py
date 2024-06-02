60408158a9cf12e52394c2adea12f769aae1c07e
Shamonti <shamonti.haque98@gmail.com>
2024-06-02 16:21:00 +0600

    Added the activity page

--- base/urls.py
+++ base/urls.py
@@ -9,11 +9,14 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('user/<str:pk>/', views.userProfile, name='user-profile'),
-    path('topics/', views.topicsPage, name='topics'),
     
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
     path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),
+
     path('update-user/', views.updateUser, name='update-user'),
+
+    path('topics/', views.topicsPage, name='topics'),
+    path('activities/', views.activityPage, name='activities')
 ]
