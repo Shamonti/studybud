1a9a538658daa4d0a310c82af12b7068e256b0dd
Shamonti <shamonti.haque98@gmail.com>
2024-06-02 11:27:07 +0600

    Added create/update room

--- base/views.py
+++ base/views.py
@@ -108,6 +108,7 @@ def userProfile(request, pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
 
     if request.method == 'POST':
         form = RoomForm(request.POST)
@@ -118,20 +119,21 @@ def createRoom(request):
             room.save()
             return redirect('home')
         
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.method == 'POST':
         form = RoomForm(request.POST,  instance=room)
         if form.is_valid:
             form.save()
             return redirect('home')
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context) 
 
 @login_required(login_url='login')
