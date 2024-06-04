53074c069be540aed51f3bfc6fc8bffdbcf12faa
Shamonti <shamonti.haque98@gmail.com>
2024-06-04 18:43:06 +0600

    added new line

--- studybud/urls.py
+++ studybud/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('api/', include('base.api.urls'))
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
