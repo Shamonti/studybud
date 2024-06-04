825ad0920d6359b9bd4a236d15c2cbd4a04cb496
Shamonti <shamonti.haque98@gmail.com>
2024-06-04 19:31:09 +0600

    Added app to static file directory

--- studybud/settings.py
+++ studybud/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/'),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, "base.static"),),
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
